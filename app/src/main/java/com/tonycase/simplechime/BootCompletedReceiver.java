3496cfe73a74554a5244cfc65daa4611af8a1bc6
tonyc10 <case.tony@gmail.com>
2015-10-06 06:56:57 -0500

    Add on upgrade receiver
    To convert 0-11 time for ending time to 0-23.

--- app/src/main/java/com/tonycase/simplechime/BootCompletedReceiver.java
+++ app/src/main/java/com/tonycase/simplechime/BootCompletedReceiver.java
@@ -10,21 +10,21 @@ import android.preference.PreferenceManager;
  * Restarts the alarm, if it's turned on, after reboot.
  */
 public class BootCompletedReceiver extends BroadcastReceiver {
-	
-	private final String TAG = getClass().getSimpleName();
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
+    private final String TAG = getClass().getSimpleName();
 
-		// if preference is On, reset alarm.
-		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context); 
-		boolean chimeOn = prefs.getBoolean(context.getString(R.string.pref_on_off), false);
-		
-		if (chimeOn) {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // if preference is On, reset alarm.
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean chimeOn = prefs.getBoolean(context.getString(R.string.pref_on_off), false);
+
+        if (chimeOn) {
             ChimeUtilities.startAlarm(context);
-		}
+        }
 
-	}
+    }
 
 
 }
